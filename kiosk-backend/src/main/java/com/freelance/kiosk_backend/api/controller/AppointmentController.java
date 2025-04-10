7147a3c4da0b7f3c00d99ac0f7ea670d0d56b7d9
Light Yagami <freelance0825@gmail.com>
2025-04-11 01:23:04 +0800

    Fix appointment endpoint

--- kiosk-backend/src/main/java/com/freelance/kiosk_backend/api/controller/AppointmentController.java
+++ kiosk-backend/src/main/java/com/freelance/kiosk_backend/api/controller/AppointmentController.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/appointment")
+@RequestMapping("/api/appointments")
 @RequiredArgsConstructor
 @Slf4j
 public class AppointmentController {
